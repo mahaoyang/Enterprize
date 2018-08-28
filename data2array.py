db7f2f58948e80d8718afadd2b097f540851278c
mahaoyang <mahydxygitlab>
2018-08-28 15:41:51 +0800

    init

--- data2array.py
+++ data2array.py
@@ -14,35 +14,11 @@ def data2array(path):
             label_list[line[0]] = line[1]
     print('label_list', len(label_list))
 
-    if not os.path.exists('train_list.pickle'):
-
-        with open(path + 'DatasetA_train_20180813/train.txt', 'r') as f:
-            train_list = dict()
-            for line in f:
-                line = line.strip('\n').split('\t')
-                train_list[line[0]] = dict()
-                train_list[line[0]]['lable'] = line[1]
-
-        for img in train_list:
-            pic = load_img(path + 'DatasetA_train_20180813/train/' + img, target_size=(64, 64))
-            pic = img_to_array(pic)
-            pic = pic.reshape((pic.shape[0], pic.shape[1], pic.shape[2]))
-            train_list[img]['array'] = pic
-
-        with open('train_list.pickle', 'wb') as f:
-            pickle.dump(train_list, f)
-
-    else:
-
-        with open('train_list.pickle', 'rb') as f:
-            train_list = pickle.load(f)
-    print('train_list', len(train_list))
-
     with open(path + 'DatasetA_train_20180813/class_wordembeddings.txt', 'r') as f:
         class_wordembeddings = dict()
         for i in f.readlines():
             ii = i.strip('\n').split('\t')
-            class_wordembeddings[int(ii[0])] = ii[1:]
+            class_wordembeddings[ii[0]] = ii[1:]
     print('class_wordembeddings', len(class_wordembeddings))
 
     with open(path + 'DatasetA_train_20180813/attributes_per_class.txt', 'r') as f:
@@ -59,10 +35,34 @@ def data2array(path):
             attribute_list[int(ii[0])] = ii[1:]
     print('attribute_list', len(attribute_list))
 
-    for i in train_list:
-        train_list[i]['label_real_name'] = label_list[i]
-        train_list[i]['label_real_name_class_wordembeddings'] = class_wordembeddings[label_list[i]]
-        train_list[i]['label_attribute'] = attributes_per_class[label_list[i]]
+    if not os.path.exists('train_list.pickle'):
+
+        with open(path + 'DatasetA_train_20180813/train.txt', 'r') as f:
+            train_list = dict()
+            for line in f:
+                line = line.strip('\n').split('\t')
+                train_list[line[0]] = dict()
+                train_list[line[0]]['lable'] = line[1]
+
+        for img in train_list:
+            pic = load_img(path + 'DatasetA_train_20180813/train/' + img, target_size=(64, 64))
+            pic = img_to_array(pic)
+            pic = pic.reshape((pic.shape[0], pic.shape[1], pic.shape[2]))
+            train_list[img]['array'] = pic
+
+        for i in train_list:
+            train_list[i]['label_real_name'] = label_list[i]
+            train_list[i]['label_real_name_class_wordembeddings'] = class_wordembeddings[label_list[i]]
+            train_list[i]['label_attribute'] = attributes_per_class[label_list[i]]
+
+        with open('train_list.pickle', 'wb') as f:
+            pickle.dump(train_list, f)
+
+    else:
+
+        with open('train_list.pickle', 'rb') as f:
+            train_list = pickle.load(f)
+    print('train_list', len(train_list))
 
     return {'lable_list': label_list, 'train_list': train_list, 'attributes_per_class': attributes_per_class,
             'attribute_list': attribute_list}
