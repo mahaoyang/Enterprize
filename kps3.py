f3a4ce665ea319bf8dc5f90ed18ac650fb994283
mahaoyang <mahydxygitlab>
2018-08-31 11:51:40 +0800

    init

--- kps3.py
+++ kps3.py
@@ -15,6 +15,7 @@ from keras import metrics
 import numpy as np
 import pickle
 from data2array import data2array
+import copy
 
 img_size = (64, 64, 3)
 # weights = 'DenseNet121_Xception_x_32.h5'
@@ -213,6 +214,7 @@ class PWNN(SimpleNN):
 
     def train(self, lr=0.000001, epochs=10, batch_size=23, rstep=10, start_rstep=-1):
         start_rstep += 1
+        model_weights = copy.deepcopy(self.model_weights)
         data = data2array(self.base_path)
         train_list = data['train_list']
         train_num = 30000
@@ -240,17 +242,19 @@ class PWNN(SimpleNN):
             model.fit(x=x[:train_num], y=[y1[:train_num], y2[:train_num], y3[:train_num], ],
                       validation_data=[x[train_num:-200], [y1[train_num:-200], y2[train_num:-200], y3[train_num:-200]]],
                       epochs=epochs, batch_size=batch_size)
+            model_weights = copy.deepcopy('%s_rstep_%s_end_%s.h5' % (self.model_weights, rstep, i))
             model.save('%s_rstep_%s_end_%s.h5' % (self.model_weights, rstep, i))
         ev = model.evaluate(x=x[-200:], y=[y1[-200:], y2[-200:], y3[-200:]], batch_size=200)
         ev = dict(zip(model.metrics_names, ev))
         print(ev)
+        return model_weights
 
-    def submit(self):
+    def submit(self, model_weights):
         data = data2array(self.base_path)
         test_list_array = data['test_list_array']
         test_list_name = data['test_list_name']
         model = self.model()
-        model.load_weights('DenseNet121_x_32_x_3.h5_rstep_7_0')
+        model.load_weights(model_weights)
         _, __, predict = model.predict(np.array(test_list_array))
         submit_lines = []
         n = 0
@@ -299,6 +303,6 @@ if __name__ == '__main__':
     # nn = MixNN(base_path=path, model_weights=weights)
     # nn.train()
     nn = PWNN(base_path=path, model_weights=weights)
-    nn.train(lr=0.000001, epochs=3, batch_size=123, rstep=7, start_rstep=-1)
-    nn.submit()
+    model_weights = nn.train(lr=0.000001, epochs=3, batch_size=123, rstep=7, start_rstep=-1)
+    nn.submit(model_weights=model_weights)
     # model_3()
